03beba1d99627183446130c6c24b0c44287b1aa7
Nik Osmolovski <nikita@osmolovskiy.com>
2026-07-08 17:40:29 +0200

    Adding user responses to the movies object using a loop and adding a check of the count property against the number of movies watched.

--- script.js
+++ script.js
@@ -10,10 +10,28 @@ let personalMovieDB = {
   privat: false,
 };
 
-let watchMovies = prompt("Один из последних просмотренных фильмов?");
-let ratingMovies = prompt("На сколько оцените его?");
-let watchMoviesTwo = prompt("Один из последних просмотренных фильмов?");
-let ratingMoviesTwo = prompt("На сколько оцените его?");
 
-personalMovieDB.movies[watchMovies] = ratingMovies;
-personalMovieDB.movies[watchMoviesTwo] = ratingMoviesTwo;
+// Добавление ответов от пользователя в объект movies
+for (let i = 0; i < 2; i++) {
+  const watchMovies = prompt("Один из последних просмотренных фильмов?");
+  const ratingMovies = prompt("На сколько оцените его?");
+
+  if (watchMovies === null || watchMovies.trim() === '' || watchMovies.length > 50) {
+    i--;
+    continue
+  };
+
+  personalMovieDB.movies[watchMovies] = ratingMovies;
+};
+
+
+// Проверка свойства count на количество просмотренных фильмов
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+}
+else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+};

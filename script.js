19fb7c871db4963eaefc4a3ceadd57a9dff89f3b
Nik Osmolovski <nikita@osmolovskiy.com>
2026-07-14 17:47:59 +0200

    Code structuring, adding functions

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
+let numberOfFilms;
 
-let personalMovieDB = {
+function start() {
+  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
+  }
+}
+
+start();
+
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
@@ -10,28 +20,59 @@ let personalMovieDB = {
   privat: false,
 };
 
-
 // Добавление ответов от пользователя в объект movies
-for (let i = 0; i < 2; i++) {
-  const watchMovies = prompt("Один из последних просмотренных фильмов?");
-  const ratingMovies = prompt("На сколько оцените его?");
+function addUserResponses() {
+  for (let i = 0; i < 2; i++) {
+    const watchMovies = prompt("Один из последних просмотренных фильмов?");
+    const ratingMovies = prompt("На сколько оцените его?");
 
-  if (watchMovies === null || watchMovies.trim() === '' || watchMovies.length > 50) {
-    i--;
-    continue
+    if (watchMovies === null || watchMovies.trim() === '' || watchMovies.length > 50) {
+      i--;
+      continue
+    };
+
+    personalMovieDB.movies[watchMovies] = ratingMovies;
   };
+}
 
-  personalMovieDB.movies[watchMovies] = ratingMovies;
-};
-
+// addUserResponses();
 
 // Проверка свойства count на количество просмотренных фильмов
-if (personalMovieDB.count < 10) {
-  console.log("Просмотрено довольно мало фильмов");
+function checkNumberMovies() {
+  if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  }
+  else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  }
+  else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  };
 }
-else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
+
+// checkNumberMovies();
+
+// Запись номера любимого жанра
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    const favoriteGenre = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+
+    if (favoriteGenre == '' || favoriteGenre == null) {
+      i--;
+      continue
+    };
+
+    personalMovieDB.genres.push(favoriteGenre);
+  };
 }
-else if (personalMovieDB.count > 30) {
-  console.log("Вы киноман");
-};
+
+writeYourGenres();
+
+// Показать базу данных в консоли
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
